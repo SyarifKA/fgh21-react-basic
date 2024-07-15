fc70d278850e2a1dd75097c845cedcfba8478c2a
Syarif09 <syarif.khalid@gmail.com>
2024-07-15 23:37:43 +0700

    fix: styling survey

--- src/component/Survey.jsx
+++ src/component/Survey.jsx
@@ -5,8 +5,8 @@ function TabelSurvey() {
         id: 1,
         name: 'syarif',
         age: '25',
-        gender: 'Laki-laki',
-        isSmoker: false,
+        gender: 'Male',
+        isSmoker: 'Yes',
         brand: ['Marlboro', 'Esse', 'Lucky Strike']
     }])
 
@@ -15,12 +15,16 @@ function TabelSurvey() {
         const name = event.target.name.value
         const age = event.target.age.value
         const gender = event.target.gender.value
-        const isSmoker = event.target.isSmoke.value
+        const smoke = event.target.isSmoke.item(0)
+        let isSmoke = false
+        if (smoke.checked) {
+            isSmoke = smoke.value
+        }
 
         const variant = event.target.variant
         let result = []
         variant.forEach((e) => {
-            if (e.checked === true) {
+            if (e.checked) {
                 result.push(e.value)
             }
         })
@@ -32,57 +36,72 @@ function TabelSurvey() {
                 name,
                 age,
                 gender,
-                isSmoker,
+                isSmoke,
                 brand
             }
         ]
         setData(newData)
-        // console.log(newData)
     }
     
 
     return (
-        <div>
-        <form onSubmit={dataResponse}>
-                <h1>Form Survey</h1>
+        <div className='survey'>
+        <h1>Form Survey</h1>
+        <form onSubmit={dataResponse} className='form-survey'>
                 <div>
                     <label htmlFor="name">Name</label>
+                    <div>
                     <input type="text" id='name' name='name'/>
+                    </div>
                 </div>
                 <div>
                     <label htmlFor="age">Age</label>
+                    <div>
                     <input type="number" id='age' name='age'/>
+                    </div>
                 </div>
                 <div>
                     <div>Gender</div>
-                    <input type="radio" id='male' name='gender'value='Laki-laki'/>
+                    <div>
+                    <input type="radio" id='male' name='gender'value='Male'/>
                     <label htmlFor="male">Male</label>
-                    <input type="radio" id='female' name='gender'value='Perempuan'/>
+                    <input type="radio" id='female' name='gender'value='Female'/>
                     <label htmlFor="female">Female</label>
+                    </div>
                 </div>
                 <div>
                     <div>Is Smoker</div>
-                    <input type="radio" id='yes' name='isSmoke' value='Ya'/>
+                    <div>
+                    <input type="radio" id='yes' name='isSmoke' value='Yes'/>
                     <label htmlFor="yes">Yes</label>
-                    <input type="radio" id='no' name='isSmoke' value='Tidak'/>
+                    <input type="radio" id='no' name='isSmoke' value='No'/>
                     <label htmlFor="no">No</label>
+                    </div>
                 </div>
                 <div>
                     <div>Cigar variant had tried</div>
+                    <div>
                     <input type="checkbox" id='marlboro' name='variant' value='marlboro'/>
                     <label htmlFor="marlboro">Marlboro</label>
+                    </div>
+                    <div>
                     <input type="checkbox" id='luckyStrike' name='variant' value='Lucky Strike'/>
                     <label htmlFor="luckyStrike">Lucky Strike</label>
+                    </div>
+                    <div>
                     <input type="checkbox" id='esse' name='variant' value='esse'/>
                     <label htmlFor="esse">Esse</label>
+                    </div>
+                    <div>
                     <input type="checkbox" id='gudangGaram' name='variant' value='Gudang Garam Filter'/>
                     <label htmlFor="gudangGaram">Gudang Garam Filter</label>
+                    </div>
                 </div>
                 <div>
                     <button type='submit'>Save</button>
                 </div>
         </form>
-        <table>
+        <table className='table-survey'>
             <thead>
                 <tr>
                 <th>Name</th>
